465ebd4c3d75f7eb4b3002f710f940873451ea40
haojunl <aaronliu98@outlook.com>
2023-06-19 01:09:33 -0700

    Tested CustomerJDBCDataAccessService

--- src/test/java/com/haojunlcode/AbstractTestcontainers.java
+++ src/test/java/com/haojunlcode/AbstractTestcontainers.java
@@ -1,13 +1,17 @@
 package com.haojunlcode;
 
-import net.minidev.json.JSONUtil;
+import com.github.javafaker.Faker;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
 
 
 @Testcontainers
@@ -46,4 +50,18 @@ public abstract class AbstractTestcontainers {
                 () -> postgreSQLContainer.getPassword()
         );
     }
+
+    private static DataSource getDataSource(){
+        return DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword())
+                .build();
+    }
+    protected static JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+    protected static final Faker FAKER = new Faker();
 }
